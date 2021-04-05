48a65383f58f40d4b6c0f9551d0182cc2357a1eb
Stephanie <s.kan@student.unsw.edu.au>
2021-04-05 12:31:53 +1000

    updating push to backend on breakchar

--- EventManager.py
+++ EventManager.py
@@ -33,7 +33,7 @@ class EventManager:
         self.history[self.historyCount] = event
         self.historyCount += 1
         
-        if self.historyCount >= self.historyThreshold:
+        if self.historyCount >= self.historyThreshold && (self.history[-1]["keyName"]=="Space" || self.history[-1]["keyName"]=="Enter"):
             self.__pushHistoryToBackend()
             
     def __pushHistoryToBackend(self):
